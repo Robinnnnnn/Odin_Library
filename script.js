e65f3bd4790e9f30805866f05e006f89a5d319c8
Your NameRobin <rlifshitz19@gmail.com>
2020-01-09 11:52:36 -0700

    Obj Constructor & Demo Objs Created

---
+++ script.js
@@ -0,0 +1,38 @@
+//Book obj constructor
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+
+}
+
+//simple prototype to display read info.
+Book.prototype.readResponse = function() {
+    let lowerCase = this.read.toLowerCase();
+    if (lowerCase === "yes") {
+        return `${this.read}, I have read ${this.title}!`;
+    } else {
+        return `${this.read}, not yet but I'm planning on it.`;
+    }
+};
+//Prototype pushes all of "Book Obj" to library array
+Book.prototype.pushToLib = function() {
+    myLibrary.push(this);
+}
+
+// Demo books - Used to populate page. Can be removed.
+const lotr1 = new Book("Lord of the Rings: The Fellow Ship of the Ring", "J.R.R Tolkien", 398, "Yes");
+const lotr2 = new Book("Lord of the Rings: The Two Towers", "J.R.R Tolkien", 400, "No");
+const lotr3 = new Book("Lord of the Rings: The Return of the King", "J.R.R Tolkien", 400, "No");
+let myLibrary = [lotr1, lotr2];
+
+
+
+
+
+// attach to user input submit button.
+
+function addBookToLibrary() {
+
+}
